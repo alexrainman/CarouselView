b7d6539aed38bf461fe86c39e6bf19e1f0b688b6
Seungkeun Lee <sngn.lee@samsung.com>
2018-03-28 13:35:56 +0900

    Add Tizen backend
     - Support Tizen platform

---
+++ Tizen/App.cs
@@ -0,0 +1,24 @@
+﻿using FFImageLoading.Forms.Tizen;
+using CarouselView.FormsPlugin.Tizen;
+
+namespace Demo.Tizen
+{
+	class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+	{
+		protected override void OnCreate()
+		{
+			base.OnCreate();
+			LoadApplication(new App());
+		}
+
+		static void Main(string[] args)
+		{
+
+			var app = new Program();
+			CarouselViewRenderer.Init();
+			CachedImageRenderer.Init(app);
+			Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+			app.Run(args);
+		}
+	}
+}
